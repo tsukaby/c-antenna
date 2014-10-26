263596d372a55c2bdd608bf32a950dddbe778e76
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2014-10-26 17:14:05 +0900

    デプロイ手順を変更

--- setup/deploy_to_server.sh
+++ setup/deploy_to_server.sh
@@ -5,8 +5,9 @@
 # ビルド
 sbt clean stage
 # リセット
-ssh -t sakura2 "sudo kill `cat c-antenna/RUNNING_PID`; sudo rm -rf ~/c-antenna"
+ssh -t sakura2 "if [[ -e c-antenna/RUNNING_PID ]]; then sudo kill `cat c-antenna/RUNNING_PID`; else echo NOT RUNNING; fi; sudo rm -rf ~/c-antenna"
 # 転送
 scp -r target/universal/stage sakura2:~/c-antenna
+scp -r setup/run_play.sh sakura2:~/c-antenna
 # 実行
-ssh -t sakura2 "cd c-antenna ; nohup sudo ./bin/c-antenna -Dhttp.port=80 >/dev/null 2>&1 &"
+ssh -t sakura2 "cd c-antenna ; chmod a+x run_play.sh ; nohup ./run_play.sh"
---
+++ setup/run_play.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo ./bin/c-antenna -Dhttp.port=80 >/dev/null 2>&1 &
