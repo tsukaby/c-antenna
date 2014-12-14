3ffcd568f8b93930f48c2bedf43544f60822ec37
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2014-12-14 17:03:16 +0900

    デプロイ方法修正。killが正しくできていなかった点を修正。

--- setup/deploy_to_server.sh
+++ setup/deploy_to_server.sh
@@ -13,7 +13,9 @@ cd ../../
 # サーバビルド
 sbt clean stage
 # リセット
-ssh -t sakura2_app "if [[ -e c-antenna/RUNNING_PID ]]; then sudo kill -9 `cat c-antenna/RUNNING_PID`; else echo NOT RUNNING; fi; sudo rm -rf ~/c-antenna"
+# アプリ停止
+ssh -t sakura2_app "/usr/local/app/c-antenna/run_play.sh stop"
+ssh -t sakura2_app "sudo rm -rf /usr/local/app/c-antenna"
 
 cp setup/run_play.sh target/universal/stage/
 
@@ -21,9 +23,9 @@ cp setup/run_play.sh target/universal/stage/
 cd target/universal/ && tar cvfz c-antenna.tar.gz stage && mv c-antenna.tar.gz ../../ && cd ../../
 
 # 転送
-scp -r c-antenna.tar.gz sakura2_app:~/
+scp -r c-antenna.tar.gz sakura2_app:/usr/local/app
 rm -f c-antenna.tar.gz
 # 解凍
-ssh -t sakura2_app "rm -rf c-antenna ; tar xvzf c-antenna.tar.gz && mv stage c-antenna"
+ssh -t sakura2_app "rm -rf /usr/local/app/c-antenna ; tar xvzf /usr/local/app/c-antenna.tar.gz && mv ./stage /usr/local/app/c-antenna"
 # 実行
-ssh -t sakura2_app "cd c-antenna && chmod a+x run_play.sh && nohup ./run_play.sh"
+ssh -t sakura2_app "cd /usr/local/app/c-antenna && chmod a+x run_play.sh && nohup ./run_play.sh start"
