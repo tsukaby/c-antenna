3fbf926d5889431fb6914f7fc3f3136514da1e28
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-05-06 03:43:15 +0900

    Delete shell scripts.

--- setup/create_table_sql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p conf/db/fixtures/default
-echo "#!Ups" > conf/db/fixtures/default/table.sql
-mysqldump -u root -d C_ANTENNA | sed '/*!/d' | sed '/--/d' >> conf/db/fixtures/default/table.sql
