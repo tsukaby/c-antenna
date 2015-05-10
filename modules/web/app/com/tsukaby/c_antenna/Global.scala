46027749d36db26439231fe5eab22ad2291b1e24
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-05-10 22:03:37 +0900

    不要なquartz削除

--- modules/web/app/com/tsukaby/c_antenna/Global.scala
+++ modules/web/app/com/tsukaby/c_antenna/Global.scala
@@ -1,11 +1,7 @@
 package com.tsukaby.c_antenna
 
-import akka.actor.Props
 import com.tsukaby.c_antenna.cache.VolatilityCache
-import play.api.Play.current
-import play.api.libs.concurrent.Akka
 import play.api.{Application, GlobalSettings}
-import us.theatr.akka.quartz.QuartzActor
 
 import scala.language.postfixOps
 
@@ -15,8 +11,6 @@ import scala.language.postfixOps
 object Global extends GlobalSettings {
   override def onStart(app: Application): Unit = {
     super.onStart(app)
-
-    val quartzActor = Akka.system.actorOf(Props[QuartzActor])
 
     // キャッシュ削除
     VolatilityCache.flushDB()
