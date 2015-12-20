243f7532c6b04ce3d6b391323ebe88e544979b57
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-12-20 22:03:42 +0900

    Spec for response code 302.

--- modules/lambda/src/test/scala/com/tsukaby/c_antenna/lambda/RssSpec.scala
+++ modules/lambda/src/test/scala/com/tsukaby/c_antenna/lambda/RssSpec.scala
@@ -48,6 +48,26 @@ class RssSpec extends Specification {
         }
       }
 
+      "when gave invalidated url (302)" >> {
+        "it returns a null" in {
+          val target = new Rss()
+          val request =
+            """
+              |{
+              |  "pageUrl": "http://tsukaby.com/"
+              |}
+            """.stripMargin
+          val in = new ByteArrayInputStream(request.getBytes("UTF-8"))
+          val out = new ByteArrayOutputStream()
+          target.findRssUrl(in, out)
+
+          val expected = """{"rssUrl":null}"""
+          val actual = new String(out.toByteArray, "UTF-8")
+
+          actual must be_===(expected)
+        }
+      }
+
       "when gave invalidated url (Page doesn't exists)" >> {
         "it returns a null" in {
           val target = new Rss()
