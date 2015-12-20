c99f8a9f1ecf2ec2789425d99518d4716f4f19b8
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-12-20 19:49:13 +0900

    Add a lambda caller.

--- modules/infrastructure/src/main/scala/com/tsukaby/c_antenna/lambda/LambdaInvoker.scala
+++ modules/infrastructure/src/main/scala/com/tsukaby/c_antenna/lambda/LambdaInvoker.scala
@@ -5,19 +5,22 @@ import com.amazonaws.services.lambda.AWSLambdaClient
 import com.amazonaws.services.lambda.invoke.{LambdaFunction, LambdaInvokerFactory}
 
 class AnalyzeRequest(var text: String) {
-  def getText:String = text
-  def setText(text: String):Unit = this.text = text
+  def getText: String = text
+
+  def setText(text: String): Unit = this.text = text
 }
 
 class AnalyzeResponse() {
   var tags: Array[String] = Array()
 
   def getTags: Array[String] = tags
+
   def setTags(tags: Array[String]): Unit = this.tags = tags
 }
 
 class ClassificationRequest(var words: Array[String] = Array()) {
   def getWords: Array[String] = words
+
   def setWords(words: Array[String]): Unit = this.words = words
 }
 
@@ -25,16 +28,33 @@ class ClassificationResponse() {
   var category: String = ""
 
   def getCategory: String = category
+
   def setCategory(category: String): Unit = this.category = category
 }
 
+class RssUrlFindRequest(var pageUrl: String) {
+  def getPageUrl: String = pageUrl
+
+  def setPageUrl(pageUrl: String): Unit = this.pageUrl = pageUrl
+}
+
+class RssUrlFindResponse() {
+  var rssUrl: String = ""
+
+  def getRssUrl: String = rssUrl
+
+  def setRssUrl(rssUrl: String): Unit = this.rssUrl = rssUrl
+}
 
 trait LambdaInvoker {
   @LambdaFunction(functionName = "Morphological_analyze")
   def analyzeMorphological(input: AnalyzeRequest): AnalyzeResponse
 
-  @LambdaFunction(functionName="CategoryClassification_classify")
+  @LambdaFunction(functionName = "CategoryClassification_classify")
   def classifyCategory(input: ClassificationRequest): ClassificationResponse
+
+  @LambdaFunction(functionName = "Rss_findRssUrl")
+  def findRssUrl(input: RssUrlFindRequest): RssUrlFindResponse
 }
 
 object LambdaInvoker {
