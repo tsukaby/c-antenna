344849e7b8d579f34760b3bdd26d62d659a62d95
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-12-24 20:37:48 +0900

    Change execution cycle.

--- modules/batch/src/main/scala/com/tsukaby/c_antenna/Main.scala
+++ modules/batch/src/main/scala/com/tsukaby/c_antenna/Main.scala
@@ -29,7 +29,7 @@ object Main {
     quartzActor ! AddCronSchedule(system.actorOf(Props[SiteCrawlActor]), "0 */10 * * * ?", SiteCrawlActor.Protocol.CrawlHatena())
 
     // サイトのサムネイル画像を生成するバッチ実行登録
-    quartzActor ! AddCronSchedule(system.actorOf(Props[SiteThumbnailActor]), "0 */20 * * * ?", SiteThumbnailActor.Protocol.Generate())
+    quartzActor ! AddCronSchedule(system.actorOf(Props[SiteThumbnailActor]), "0 */10 * * * ?", SiteThumbnailActor.Protocol.Generate())
 
     // RSSを収集するバッチ実行登録
     quartzActor ! AddCronSchedule(system.actorOf(Props[RssCrawlActor]), "0 */3 * * * ?", RssCrawlActor.Protocol.CrawlAll())
