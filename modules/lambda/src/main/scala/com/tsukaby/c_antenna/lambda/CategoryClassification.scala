3878d2f32395e802eae1654a6b5e247468703584
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2016-01-01 00:50:05 +0900

    Refactoring.

--- modules/lambda/src/main/scala/com/tsukaby/c_antenna/lambda/CategoryClassification.scala
+++ modules/lambda/src/main/scala/com/tsukaby/c_antenna/lambda/CategoryClassification.scala
@@ -1,7 +1,9 @@
 package com.tsukaby.c_antenna.lambda
 
-import java.io.{InputStream, OutputStream}
+import java.io.{BufferedReader, InputStream, InputStreamReader, OutputStream}
 
+import com.github.tototoshi.csv.CSVReader
+import com.tsukaby.bayes.classifier.{BayesClassifier, Classification}
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.json4s.native.Serialization
@@ -15,12 +17,40 @@ case class ClassificationResponse(category: String)
 class CategoryClassification {
   implicit val formats = DefaultFormats
 
+  val is = this.getClass.getClassLoader.getResourceAsStream("categories_training_data.csv")
+  val br = new BufferedReader(new InputStreamReader(is, "UTF-8"))
+  val reader = CSVReader.open(br)
+
+  val bayes = new BayesClassifier[String, String]()
+
+  // Learning
+  learn()
+
   def classify(input: InputStream, output: OutputStream): Unit = {
     val req = parse(Source.fromInputStream(input)(Codec.UTF8).mkString).extract[ClassificationRequest]
-    val response = ClassificationResponse(CategoryClassificationService.classify(req.words))
+    val response = ClassificationResponse(classifyBy(req.words))
     val responseStr = Serialization.write(response)
 
     val result = responseStr.getBytes("UTF-8")
     output.write(result)
+  }
+
+  private def learn() = {
+    reader.all().map { line =>
+      (line.head, line(1))
+    } groupBy (_._1) foreach { case (category, learningData) =>
+      bayes.learn(category, learningData.map(_._2))
+    }
+  }
+
+
+  /**
+    * 引数で与えた形態素解析済みのテキストから該当するカテゴリを取得します。
+    * @param words 単語単位に形態素解析されている解析対象テキスト
+    * @return 判別されたカテゴリ
+    */
+  private def classifyBy(words: Traversable[String]): String = {
+    val result: Option[Classification[String, String]] = bayes.classify(words)
+    result.map(_.category).getOrElse("Unknown")
   }
 }
--- modules/lambda/src/main/scala/com/tsukaby/c_antenna/lambda/Morphological.scala
+++ modules/lambda/src/main/scala/com/tsukaby/c_antenna/lambda/Morphological.scala
@@ -2,10 +2,12 @@ package com.tsukaby.c_antenna.lambda
 
 import java.io.{InputStream, OutputStream}
 
+import org.atilika.kuromoji.{Token, Tokenizer}
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.json4s.native.Serialization
 
+import scala.collection.JavaConverters._
 import scala.io.{Codec, Source}
 
 case class AnalyzeRequest(text: String)
@@ -15,12 +17,48 @@ case class AnalyzeResponse(tags: Seq[String])
 class Morphological {
   implicit val formats = DefaultFormats
 
+  private val tokenizer: Tokenizer = {
+    val input = this.getClass.getClassLoader.getResourceAsStream("my-userdict.txt")
+    val tokenizer = Tokenizer.builder().userDictionary(input).build()
+    input.close()
+    tokenizer
+  }
+
   def analyze(input: InputStream, output: OutputStream): Unit = {
     val textInfo = parse(Source.fromInputStream(input)(Codec.UTF8).mkString).extract[AnalyzeRequest]
-    val response = AnalyzeResponse(MorphologicalService.getTags(textInfo.text).map(_._1))
+    val response = AnalyzeResponse(getTags(textInfo.text).map(_._1))
     val responseStr = Serialization.write(response)
 
     val result = responseStr.getBytes("UTF-8")
     output.write(result)
+  }
+
+  /**
+    * 引数で指定したテキストからタグを生成し、取得します。
+    * @param text タグにするテキスト
+    * @return 頻度順にソートされたタグのリスト
+    */
+  def getTags(text: String): Seq[(String, Int)] = {
+
+    val tokensNormal: List[Token] = tokenizer.tokenize(text).asScala.toList
+
+    // 必要な単語だけに絞る
+    val filteredTokens = tokensNormal filter { token =>
+      val first = token.getAllFeaturesArray.array(0)
+      // 品詞2つ目は存在しない場合があるため、制御
+      val second = if (token.getAllFeaturesArray.length < 2) "" else token.getAllFeaturesArray.array(1)
+      // 辞書登録されている用語または、未知語でなく名詞だけに絞る
+      token.isUser || (first == "名詞" && second != "サ変接続" && second != "数" && second != "接尾" && second != "代名詞" && second != "非自立")
+    } filterNot { token =>
+      // アルファベット１文字は弾く
+      token.getSurfaceForm.matches( """[a-zA-Z]""")
+    }
+
+    // 名称だけのリストに変換
+    val words = filteredTokens map (x => x.getSurfaceForm)
+
+    // 出現頻度のMapに変換
+    words.groupBy(identity).mapValues(_.size).toSeq.sortWith(_._2 > _._2)
+
   }
 }
