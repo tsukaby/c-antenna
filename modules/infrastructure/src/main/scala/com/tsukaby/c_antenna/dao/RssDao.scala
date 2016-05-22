8ca4174f0f817954bda79ef7d8e0d024c4000757
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2016-05-22 18:59:07 +0900

    fix

--- modules/infrastructure/src/main/scala/com/tsukaby/c_antenna/dao/RssDao.scala
+++ modules/infrastructure/src/main/scala/com/tsukaby/c_antenna/dao/RssDao.scala
@@ -1,6 +1,6 @@
 package com.tsukaby.c_antenna.dao
 
-import java.io.{BufferedReader, InputStreamReader, Reader}
+import java.io.{BufferedReader, ByteArrayInputStream, InputStreamReader, Reader}
 import java.net.URL
 
 import com.rometools.rome.feed.synd.SyndFeed
@@ -8,6 +8,7 @@ import com.rometools.rome.io.SyndFeedInput
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.io.Source
 
 /**
  * informaライブラリを利用してRSSを取得するクラスです。
@@ -25,7 +26,11 @@ trait RssDao extends BaseDao {
       val conn = tmp.openConnection()
       // PC Chrome
       conn.setRequestProperty("User-agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36")
-      val reader: Reader = new BufferedReader(new InputStreamReader(conn.getInputStream))
+      // 特定の文字削除
+      val convertedStr: String = Source.fromInputStream(conn.getInputStream).getLines()
+        .map(_.replaceAll("[\\00-\\x08\\x0a-\\x1f\\x7f]", "")).mkString("\n")
+      val convertedIs: ByteArrayInputStream = new ByteArrayInputStream(convertedStr.getBytes("UTF-8"))
+      val reader: Reader = new BufferedReader(new InputStreamReader(convertedIs))
 
       val in = new SyndFeedInput()
       val feed = in.build(reader)
